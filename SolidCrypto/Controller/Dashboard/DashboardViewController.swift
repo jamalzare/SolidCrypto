a1041c96fb0f009f2f175c3646a0c93e8d70ec55
jamal zare <jamal.zare@solidict.com>
2023-06-09 12:31:04 +0330

    Start Dashboard

---
+++ SolidCrypto/Controller/Dashboard/DashboardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardViewController.swift
+//  SolidCrypto
+//
+//  Created by Jamal on 6/9/23.
+//
+
+import Foundation
+import UIKit
+
+class DashboardViewController: UIViewController {
+    
+    var investments: [AddInvestment] = []
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+}
